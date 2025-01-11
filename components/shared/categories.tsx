dac7afe075bf1ea178d02e0df27a9235ef04595e
githorder <mygit.2018@gmail.com>
2025-01-12 01:25:42 +0500

    feat: categories v1

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -1,11 +1,46 @@
 import React from "react";
 
+import { cn } from "@/lib/utils";
+
 interface Props {
   className?: string;
 }
 
+const cats = [
+  "Пиццы",
+  "Комбо",
+  "Закуски",
+  "Коктели",
+  "Кофе",
+  "Напитки",
+  "Десерты",
+  "Десерты",
+];
+
+const activeIndex = 0;
+
 const Categories: React.FC<Props> = ({ className }) => {
-  return <div className={className}></div>;
+  return (
+    <div
+      className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
+    >
+      {cats.map((cat, index) => {
+        return (
+          <a
+            key={index}
+            className={cn(
+              "flex items-center px-5 rounded-2xl font-bold h-11",
+              index === activeIndex
+                ? "bg-white text-primary shadow-md shadow-gray-200"
+                : ""
+            )}
+          >
+            <button>{cat}</button>
+          </a>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Categories;
