8d8c8ff36c8773660f722adeb03c184a19a75ab5
githorder <mygit.2018@gmail.com>
2025-01-12 20:02:56 +0500

    feat: range slider v1

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 
 import { Title } from "./title";
 import { FilterCheckbox } from "./filter-checkbox";
+import { RangeSlider } from "./range-slider";
+import { Input } from "../ui";
 
 interface Props {
   className?: string;
@@ -17,6 +19,27 @@ export const Filters: React.FC<Props> = ({ className }) => {
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
+      </div>
+
+      <div className="border-y border-y-neutral-100 mt-5 py-6 pb-7">
+        <p className="mb-3 font-bold">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input
+            type="number"
+            placeholder="0"
+            min={0}
+            max={1000}
+            defaultValue={0}
+          />
+          <Input
+            type="number"
+            placeholder="1000"
+            min={100}
+            max={1000}
+            defaultValue={100}
+          />
+        </div>
+        <RangeSlider min={0} max={1000} step={10} value={[0, 1000]} />
       </div>
     </div>
   );
---
+++ components/shared/range-slider.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import React from "react";
+
+import * as SliderPrimitive from "@radix-ui/react-slider";
+
+import { cn } from "@/lib/utils";
+
+type SliderProps = {
+  className?: string;
+  min: number;
+  max: number;
+  step: number;
+  formatLabel?: (value: number) => string;
+  value?: number[] | readonly number[];
+  onValueChange?: (values: number[]) => void;
+};
+
+const RangeSlider = React.forwardRef(
+  (
+    {
+      className,
+      min,
+      max,
+      step,
+      formatLabel,
+      value,
+      onValueChange,
+      ...props
+    }: SliderProps,
+    ref
+  ) => {
+    const initialValue = Array.isArray(value) ? value : [min, max];
+    const [localValues, setLocalValues] = React.useState(initialValue);
+
+    React.useEffect(() => {
+      // Update localValues when the external value prop changes
+      setLocalValues(Array.isArray(value) ? value : [min, max]);
+    }, [min, max, value]);
+
+    const handleValueChange = (newValues: number[]) => {
+      setLocalValues(newValues);
+      if (onValueChange) {
+        onValueChange(newValues);
+      }
+    };
+
+    return (
+      <SliderPrimitive.Root
+        ref={ref as React.RefObject<HTMLDivElement>}
+        min={min}
+        max={max}
+        step={step}
+        value={localValues}
+        onValueChange={handleValueChange}
+        className={cn(
+          "relative flex w-full touch-none select-none mb-6 items-center",
+          className
+        )}
+        {...props}
+      >
+        <SliderPrimitive.Track className="relative bg-primary/20 rounded-full w-full h-1 overflow-hidden grow">
+          <SliderPrimitive.Range className="absolute bg-primary h-full" />
+        </SliderPrimitive.Track>
+        {localValues.map((value, index) => (
+          <React.Fragment key={index}>
+            <div
+              className="absolute text-center"
+              style={{
+                left: `calc(${((value - min) / (max - min)) * 100}% + 0px)`,
+                top: `10px`,
+              }}
+            >
+              <span className="text-sm">
+                {formatLabel ? formatLabel(value) : value}
+              </span>
+            </div>
+            <SliderPrimitive.Thumb className="block border-primary/50 bg-white disabled:opacity-50 shadow border rounded-full focus-visible:ring-1 focus-visible:ring-ring w-4 h-4 transition-colors disabled:pointer-events-none focus-visible:outline-none" />
+          </React.Fragment>
+        ))}
+      </SliderPrimitive.Root>
+    );
+  }
+);
+
+RangeSlider.displayName = SliderPrimitive.Root.displayName;
+
+export { RangeSlider };
